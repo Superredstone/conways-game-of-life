645815a8bb85ae9aab53a15f2fda73111a2325f7
Superredstone <patrickcanal3@gmail.com>
2024-03-03 15:53:53 +0100

    Added mouse controls

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
-use raylib::prelude::*;
+use std::{fs::create_dir, usize};
+
+use raylib::{ffi::GetFrameTime, prelude::*};
 
 const WINDOW_SIZE: Vector2 = Vector2::new(800.0, 800.0);
 const GRID_SIZE: Vector2 = Vector2::new(50.0, 50.0);
 const CREATURE_SIZE_X: usize = GRID_SIZE.x as usize;
 const CREATURE_SIZE_Y: usize = GRID_SIZE.y as usize;
 const CELL_SIZE: Vector2 = Vector2::new(WINDOW_SIZE.x / GRID_SIZE.x, WINDOW_SIZE.y / GRID_SIZE.y);
+
+struct Config {
+    do_next_frame: bool,
+    cycle_counter: u32,
+    speed: f32,
+    auto_run: bool,
+    frame_counter: u32,
+}
+
+struct Vector2i {
+    x: i32,
+    y: i32,
+}
 
 fn main() {
     let (mut rl, thread) = raylib::init()
@@ -26,12 +41,51 @@ fn main() {
     creatures[36][25] = true;
     creatures[36][26] = true;
 
-    let mut do_next_frame = false;
-    let mut frame_number = 0;
+    let mut config = Config {
+        do_next_frame: false,
+        cycle_counter: 0,
+        speed: 900.0,
+        auto_run: false,
+        frame_counter: 0,
+    };
 
+    // let mut do_next_frame = false;
+    // let mut cycle_counter = 0;
+    // let mut speed = 900;
+    // let mut auto_run = false;
+    // let mut frame_counter = 0;
+    //
     while !rl.window_should_close() {
+        let frame_time = unsafe { GetFrameTime() };
+        let mouse_position: Vector2i = Vector2i {
+            x: rl.get_mouse_x(),
+            y: rl.get_mouse_y(),
+        };
         if rl.is_key_pressed(KeyboardKey::KEY_D) {
-            do_next_frame = true;
+            config.do_next_frame = true;
+        }
+        if rl.is_key_pressed(KeyboardKey::KEY_SPACE) {
+            if config.auto_run {
+                config.auto_run = false;
+            } else {
+                config.auto_run = true;
+            }
+        }
+        if rl.is_key_down(KeyboardKey::KEY_S) && config.speed >= 1.0 {
+            config.speed -= 10.0 * frame_time;
+        }
+        if rl.is_key_down(KeyboardKey::KEY_W) {
+            config.speed += 10.0 * frame_time;
+        }
+        if rl.is_mouse_button_pressed(MouseButton::MOUSE_LEFT_BUTTON) {
+            creatures[(mouse_position.x / CELL_SIZE.x as i32) as usize]
+                [(mouse_position.y / CELL_SIZE.y as i32) as usize] = !creatures
+                [(mouse_position.x / CELL_SIZE.x as i32) as usize]
+                [(mouse_position.y / CELL_SIZE.y as i32) as usize];
+        }
+        if rl.is_key_pressed(KeyboardKey::KEY_R) {
+            creatures = [(); CREATURE_SIZE_X as usize]
+                .map(|_| [(); CREATURE_SIZE_Y as usize].map(|_| false));
         }
 
         let mut d = rl.begin_drawing(&thread);
@@ -42,16 +96,41 @@ fn main() {
         draw_grid(&mut d);
 
         d.draw_text(
-            &format!("Life cycles: {}", frame_number),
+            &format!("Life cycles: {}", config.cycle_counter),
             10,
             10,
             30,
             Color::BLACK,
         );
+        d.draw_text(
+            &format!("Autorun speed: {}", config.speed),
+            10,
+            40,
+            30,
+            Color::BLACK,
+        );
+        d.draw_text(
+            &format!(
+                "X: {} Y: {}",
+                mouse_position.x / CELL_SIZE.x as i32,
+                mouse_position.y / CELL_SIZE.y as i32
+            ),
+            10,
+            40 + 30,
+            30,
+            Color::BLACK,
+        );
 
-        if !do_next_frame {
+        if !config.do_next_frame && !config.auto_run {
             continue;
         }
+
+        if config.frame_counter <= config.speed as u32 && config.auto_run {
+            config.frame_counter += 1;
+            continue;
+        }
+
+        config.frame_counter = 0;
 
         // --- Update ---
         let mut new_array: [[bool; CREATURE_SIZE_Y]; CREATURE_SIZE_X] =
@@ -60,8 +139,8 @@ fn main() {
         update_creatures(&creatures, &mut new_array);
         creatures = new_array;
 
-        frame_number += 1;
-        do_next_frame = false;
+        config.cycle_counter += 1;
+        config.do_next_frame = false;
     }
 }
 
@@ -123,7 +202,7 @@ fn count_neighbour_cell(
         neighbours += 1;
     }
 
-    return neighbours;
+    neighbours
 }
 
 // fn count_neighbour_cell(
